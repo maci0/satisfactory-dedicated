e30e5afa3dcd6f59d3dfe34fee8cc4caf33ca242
Marcel Wysocki <maci.stgn@gmail.com>
2021-10-29 11:10:13 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=${STEAMAPPDIR}/linux64:$LD_LIBRARY_PATH
+
+bash ${STEAMCMDDIR}/steamcmd.sh  +login anonymous +force_install_dir ${STEAMAPPDIR} +app_update ${STEAMAPPID} validate +quit
+
+test -d ~/.config/Epic/FactoryGame/Saved || mkdir -p ~/.config/Epic/FactoryGame/Saved
+test -d ${STEAMAPPDIR}/saves || mkdir ${STEAMAPPDIR}/saves
+test -L ~/.config/Epic/FactoryGame/Saved/SaveGames || ln -s ${STEAMAPPDIR}/saves ~/.config/Epic/FactoryGame/Saved/SaveGames
+
+exec ${STEAMAPPDIR}/FactoryServer.sh"
