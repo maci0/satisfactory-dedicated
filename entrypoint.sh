d0d6a83d90d5b838745193331b4ea75445595203
Marcel Wysocki <maci.stgn@gmail.com>
2021-10-29 15:15:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,9 @@ test -d ~/.config/Epic/FactoryGame/Saved || mkdir -p ~/.config/Epic/FactoryGame/
 test -d ${STEAMAPPDIR}/saves || mkdir ${STEAMAPPDIR}/saves
 test -L ~/.config/Epic/FactoryGame/Saved/SaveGames || ln -s ${STEAMAPPDIR}/saves ~/.config/Epic/FactoryGame/Saved/SaveGames
 
-exec ${STEAMAPPDIR}/FactoryServer.sh
+#exec ${STEAMAPPDIR}/FactoryServer.sh
+
+UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
+chmod +x "$UE4_PROJECT_ROOT/Engine/Binaries/Linux/UE4Server-Linux-Shipping"
+exec "$UE4_PROJECT_ROOT/Engine/Binaries/Linux/UE4Server-Linux-Shipping" FactoryGame "$@"
