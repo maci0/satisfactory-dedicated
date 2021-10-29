2aafcade5c0c15eb71dd5612637b90f51d49a2da
Marcel Wysocki <maci.stgn@gmail.com>
2021-10-29 12:11:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 mkdir -p data
 
-podman run --rm -p 15000:15000/udp -p 7777:7777/udp -p 15777:15777/udp -d \
+podman run -p 15000:15000/udp -p 7777:7777/udp -p 15777:15777/udp -d \
 --name=satisfactory -v $PWD/data:/home/steam/Satisfactory-dedicated:Z \
 --userns=keep-id satisfactory
