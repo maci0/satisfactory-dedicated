549b25b764604d08764a142a4c6b5665ae706c3f
Marcel Wysocki <maci.stgn@gmail.com>
2021-10-29 11:25:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-mkdir data
+mkdir -p data/steamcmd
+mkdir -p data/satisfactory
+
 podman run --rm -p 15000:15000/udp -p 7777:7777/udp -p 15777:15777/udp -d \
---name=satisfactory -v $PWD/data:/home/steam/Satisfactory-dedicated:Z satisfactory
+--name=satisfactory -v $PWD/data:/home/steam/Satisfactory-dedicated:Z \
+$PWD/steamcmd:/home/steam/steamcd:Z --user 1000 satisfactory
